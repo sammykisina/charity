ab73bb8a8dd48c3fdc31f7688d891d13625474a4
paul wanyik <sammy.k.mutua@gmail.com>
2022-12-09 06:05:45 +0300

    Adding whitespace-nowrap On Button

--- src/components/ui-html/Button.tsx
+++ src/components/ui-html/Button.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import type { ReactNode, FC } from "react";
 
 const button_styles = cva(
-  "flex items-center justify-center px-4 py-2 rounded-full  focus:outline-none w-fit",
+  "flex items-center justify-center px-4 py-2 rounded-full  focus:outline-none w-fit whitespace-nowrap",
   {
     variants: {
       intent: {
