d7d3edc9af5842d1d4354906466e7134c28d60a9
paul wanyik <sammy.k.mutua@gmail.com>
2022-12-14 15:22:12 +0300

    Mobile UI Fix p4

--- src/components/pages-sections/fundraising/Organization.tsx
+++ src/components/pages-sections/fundraising/Organization.tsx
@@ -92,7 +92,7 @@ const Organization = () => {
       </div>
 
       {/* Fundraising Table */}
-      <section className=" h-[38rem] w-full rounded-t-[2rem] rounded-b-[1rem]  bg-white py-6 px-4 xs:h-[36rem]">
+      <section className=" h-[40rem] w-full rounded-t-[2rem] rounded-b-[1rem]  bg-white py-6 px-4 xs:h-[36rem]">
         <Table
           data={getFundraisings()}
           columns={columns}
